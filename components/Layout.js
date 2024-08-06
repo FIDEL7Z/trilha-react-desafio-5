9b7dcf9f5b9b0a838a90720148027b0b6abb303d
Jefferson Fidelis <junior.fidelis.3386@gmail.com>
2024-08-06 14:06:32 -0300

    up

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { useEffect } from 'react';
 import styles from './Layout.module.css';
 
+
 export function GradientBackground({ variant, className }) {
   const classes = classNames(
     {
